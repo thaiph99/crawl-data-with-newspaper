feaab6f6269c1fcb6b000e72004348e7eb777435
thaiph99 <thaiph99@gmail.com>
2021-04-01 15:38:10 +0700

    update crawl.py

--- crawl.py
+++ crawl.py
@@ -1,4 +1,6 @@
 from bs4 import BeautifulSoup
+from newspaper import Article
+
 import requests
 
 import re
@@ -24,16 +26,21 @@ print(*dict_url.keys(), sep='\n')
 dict_title = {}
 
 path = 'datacrawled/'
-
+cnt = 0
 for url in dict_url.keys():
+
     article = Article(url)
     article.download()
     article.parse()
     article.nlp()
     text = article.text.replace('\n', '.\n')
     title = article.title
-    summary = article.sumary
+    summary = article.summary
     url = article.url
-    with open(path+title+'txt', 'r') as f:
-        f.write
-
+    if title not in dict_title.keys():
+        dict_title[title] = 1
+        cnt += 1
+        with open(path+'article'+str(cnt)+'.txt', 'w') as f:
+            f.write(title+'\n')
+            f.write(url+'\n')
+            f.write(summary+'\n')
